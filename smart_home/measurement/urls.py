927e313903d5651940312f2f1deed32cbe30569f
Илья <92798782+ilya31415@users.noreply.github.com>
2022-04-20 12:54:42 +0300

    Update urls.py

--- smart_home/measurement/urls.py
+++ smart_home/measurement/urls.py
@@ -5,7 +5,8 @@ from measurement.views import SensorView, MeasurementView, SensorRetrieve, Senso
 urlpatterns = [
 
     path('sensors/', SensorView.as_view()),  # список датчиков get и создать датчик при post
-    path('sensors/<pk>/', SensorChange.as_view()),  # изменить датчик/получить информацию по конкретному датчику
+    path('sensors/updat/<pk>/', SensorChange.as_view()),  # изменить датчик
+    path('sensors/<pk>/', SensorRetrieve.as_view()),  # получить информацию по конкретному датчику
     path('measurements/', MeasurementView.as_view()),  # Добавить измерение post и вывести все измерения get
 
 ]
